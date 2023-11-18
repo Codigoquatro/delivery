e18aa8b1119367d30db33bba140343e35cae87f0
Codigoquatro <codigoquatro2022@gmail.com>
2023-11-18 17:24:39 -0300

    update

--- index.php
+++ index.php
@@ -46,21 +46,21 @@
                       <button class="rd-navbar-basket fl-bigmug-line-shopping198" data-rd-navbar-toggle=".cart-inline"><span>2</span></button>
                       <div class="cart-inline">
                         <div class="cart-inline-header">
-                          <h5 class="cart-inline-title">In cart:<span> 2</span> Products</h5>
-                          <h6 class="cart-inline-title">Total price:<span> $800</span></h6>
+                          <h5 class="cart-inline-title">Carrinho:<span> 2</span> Produtos</h5>
+                          <h6 class="cart-inline-title">Valor total:<span> R$800,00</span></h6>
                         </div>
                         <div class="cart-inline-body">
                           <div class="cart-inline-item">
                             <div class="unit align-items-center">
                               <div class="unit-left"><a class="cart-inline-figure" href="#"><img src="images/product-mini-1-108x100.png" alt="" width="108" height="100"/></a></div>
                               <div class="unit-body">
-                                <h6 class="cart-inline-name"><a href="#">Blueberries</a></h6>
+                                <h6 class="cart-inline-name"><a href="#">Nome Produtos</a></h6>
                                 <div>
                                   <div class="group-xs group-inline-middle">
                                     <div class="table-cart-stepper">
                                       <input class="form-input" type="number" data-zeros="true" value="1" min="1" max="1000">
                                     </div>
-                                    <h6 class="cart-inline-title">$550</h6>
+                                    <h6 class="cart-inline-title">R$0,00</h6>
                                   </div>
                                 </div>
                               </div>
@@ -70,13 +70,13 @@
                             <div class="unit align-items-center">
                               <div class="unit-left"><a class="cart-inline-figure" href="#"><img src="images/product-mini-2-108x100.png" alt="" width="108" height="100"/></a></div>
                               <div class="unit-body">
-                                <h6 class="cart-inline-name"><a href="#">Avocados</a></h6>
+                                <h6 class="cart-inline-name"><a href="#">Nome Produto</a></h6>
                                 <div>
                                   <div class="group-xs group-inline-middle">
                                     <div class="table-cart-stepper">
                                       <input class="form-input" type="number" data-zeros="true" value="1" min="1" max="1000">
                                     </div>
-                                    <h6 class="cart-inline-title">$250</h6>
+                                    <h6 class="cart-inline-title">R$250,00</h6>
                                   </div>
                                 </div>
                               </div>
@@ -84,7 +84,7 @@
                           </div>
                         </div>
                         <div class="cart-inline-footer">
-                          <div class="group-sm"><a class="button button-md button-default-outline-2 button-wapasha" href="#">Go to cart</a><a class="button button-md button-primary button-pipaluk" href="#">Checkout</a></div>
+                          <div class="group-sm"><a class="button button-md button-default-outline-2 button-wapasha" href="#">Produtos</a><a class="button button-md button-primary button-pipaluk" href="#">Pagamentos</a></div>
                         </div>
                       </div>
                     </div><a class="rd-navbar-basket rd-navbar-basket-mobile fl-bigmug-line-shopping198" href="#"><span>2</span></a>
@@ -93,7 +93,7 @@
                       <button class="rd-navbar-search-toggle" data-rd-navbar-toggle=".rd-navbar-search"><span></span></button>
                       <form class="rd-search" action="#">
                         <div class="form-wrap">
-                          <label class="form-label" for="rd-navbar-search-form-input">Search...</label>
+                          <label class="form-label" for="rd-navbar-search-form-input">Procurar...</label>
                           <input class="rd-navbar-search-form-input form-input" id="rd-navbar-search-form-input" type="text" name="search">
                           <button class="rd-search-form-submit fl-bigmug-line-search74" type="submit"></button>
                         </div>
@@ -118,7 +118,7 @@
                 </div>
                 <div class="rd-navbar-project rd-navbar-modern-project">
                   <div class="rd-navbar-project-modern-header">
-                    <h4 class="rd-navbar-project-modern-title">Get in Touch</h4>
+                    <h4 class="rd-navbar-project-modern-title">Nossos contatos</h4>
                     <div class="rd-navbar-project-hamburger" data-multitoggle=".rd-navbar-main" data-multitoggle-blur=".rd-navbar-wrap" data-multitoggle-isolate>
                       <div class="project-close"><span></span><span></span></div>
                     </div>
@@ -134,8 +134,8 @@
                       <ul class="rd-navbar-modern-contacts">
                         <li>
                           <div class="unit unit-spacing-sm">
-                            <div class="unit-left"><span class="icon fa fa-phone"></span></div>
-                            <div class="unit-body"><a class="link-phone" href="tel:#">+1 323-913-4688</a></div>
+                            <div class="unit-left"><span class="icon fab fa-whatsapp"></span></div>
+                            <div class="unit-body"><a target="_blank" class="link-phone" href="http://api.whatsapp.com/send?1=pt_BR&phone=5585">+1 323-913-4688</a></div>
                           </div>
                         </li>
                         <li>
@@ -151,12 +151,10 @@
                           </div>
                         </li>
                       </ul>
-                      <ul class="list-inline rd-navbar-modern-list-social">
-                        <li><a class="icon fa fa-facebook" href="#"></a></li>
-                        <li><a class="icon fa fa-twitter" href="#"></a></li>
-                        <li><a class="icon fa fa-google-plus" href="#"></a></li>
-                        <li><a class="icon fa fa-instagram" href="#"></a></li>
-                        <li><a class="icon fa fa-pinterest" href="#"></a></li>
+                      <ul class="list-inline rd-navbar-modern-list-social" style="text-align: center;">
+                        <li><a class="icon fa fa-facebook" href="#"></a></li>                                               
+                        <li><a class="icon fa fa-instagram" href="#"></a></li>  
+                        <li><a class="icon fab fa-youtube" href="#"></a></li>                                                                                           
                       </ul>
                     </div>
                   </div>
