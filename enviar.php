9201eb886b81124e4b3042976fa7f39300ec8cd1
Codigoquatro <codigoquatro2022@gmail.com>
2023-12-29 10:00:39 -0300

    create file config.php

--- enviar.php
+++ enviar.php
@@ -1,29 +1,10 @@
 <?php
 include_once("config.php");
 
-// Certifique-se de que $email_adm está definido em config.php
-if (isset($email_adm)) {
-    // Use $email_adm como remetente
-    $remetente = $email_adm;
+$mensagem = mb_convert_encoding('Nome: ' . $_POST['nome'] . "\r\n" . "\r\n" . 'Telefone: ' . $_POST['telefone'] . "\r\n" . "\r\n" . 'Mensagem: ' . "\r\n" . "\r\n" . $_POST['mensagem'], 'UTF-8', 'ISO-8859-1');
 
-    // Assunto do e-mail
-    $assunto = 'Contato do Site';
+$dest = $_POST['email'];
+$cabecalhos = "From: " .$dest;
 
-    // Construa a mensagem com quebras de linha
-    $mensagem = "Nome: {$_POST['name']}\r\n\r\nTelefone: {$_POST['telefone']}\r\n\r\nMensagem:\r\n\r\n{$_POST['mensagem']}";
-
-    // Destinatário do e-mail
-    $dest = $_POST['email'];
-
-    // Cabeçalhos
-    $cabecalhos = "From: $dest\r\n";
-    $cabecalhos .= "Reply-To: $dest\r\n";
-    $cabecalhos .= "Content-Type: text/plain; charset=utf-8\r\n";
-
-    // Envie o e-mail
-    mail($remetente, $assunto, $mensagem, $cabecalhos);
-} else {
-    // Se $email_adm não estiver definido, lide com isso de acordo com sua lógica
-    echo "Erro: Endereço de e-mail do administrador não configurado.";
-}
+mail($para, $assunto, $mensagem, $cabecalhos);
 ?>
