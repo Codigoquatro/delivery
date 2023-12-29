b031e5b9503c5604b4ad130122a667b7baddc546
Codigoquatro <codigoquatro2022@gmail.com>
2023-12-29 10:17:41 -0300

    create file config.php

--- enviar.php
+++ enviar.php
@@ -1,6 +1,9 @@
 <?php
 include_once("config.php");
 
+$remetente = $email_adm;
+$assunto = 'Contato do Site';
+
 $mensagem = mb_convert_encoding('Nome: ' . $_POST['nome'] . "\r\n" . "\r\n" . 'Telefone: ' . $_POST['telefone'] . "\r\n" . "\r\n" . 'Mensagem: ' . "\r\n" . "\r\n" . $_POST['mensagem'], 'UTF-8', 'ISO-8859-1');
 
 $dest = $_POST['email'];
