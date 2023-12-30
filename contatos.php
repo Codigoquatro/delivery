c9c4dbcb5ac14e3448569416c6ff91a478e17e5f
Codigoquatro <codigoquatro2022@gmail.com>
2023-12-30 08:30:46 -0300

    create file config.php

--- contatos.php
+++ contatos.php
@@ -94,3 +94,5 @@
         </div>
       </section>
       <?php include_once("footer.php")?>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
+      <script src="js/mascaras.js"></script>
